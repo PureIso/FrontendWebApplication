6c65a0bd33143123a0fa81cb2873ae4b5aa08678
Ola <olaegby@live.com>
2024-10-21 13:01:02 +0300

    Integrated tailwind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,33 @@
+// ./app/app.component.ts
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ThemeService } from './core/services/theme.service';
+import { DashboardComponent } from './features/dashboard/components/dashboard.component';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
+  imports: [DashboardComponent],
+  standalone: true,
 })
 export class AppComponent {
-  title = 'FrontendWebApplication';
+  title = 'Your Angular App';
+
+  constructor(private themeService: ThemeService) {
+    // Set initial theme
+    this.themeService.setTheme(this.themeService.getTheme());
+  }
+
+ // Function to toggle between light and dark themes
+ toggleTheme() {
+  this.themeService.toggleTheme();
+}
+
+// Function to toggle the visibility of the mobile menu
+toggleMobileMenu() {
+  const menu = document.getElementById('mobile-menu');
+  if (menu) {
+    menu.classList.toggle('hidden'); // Toggle the 'hidden' class to show/hide the menu
+  }
+}
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './features/dashboard/components/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: DashboardComponent }, // Define your route
+  // Add other routes here
+];
